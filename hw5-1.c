b441dcf426ab286fd5d1ade1f0f6405ce0ad1908
112310204 <146065554+li-weisheng@users.noreply.github.com>
2023-10-31 15:15:09 +0800

    Update hw5-1.c

--- hw5-1.c
+++ hw5-1.c
@@ -1,1 +1,10 @@
-https://classroom.github.com/a/3lByUPEW
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float f,c;
+    scanf ("%f",&c);
+    f = c*1.8 + 32 ;
+    printf("%.1f" ,f);
+}

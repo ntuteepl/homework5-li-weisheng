99c4a21b092a1821f433d46faa7b6fefb167a126
112310204 <146065554+li-weisheng@users.noreply.github.com>
2023-10-31 21:58:32 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int money , t;
+    float d;
+    scanf("%d%d",&money,&t);
+    if(t <= 60)
+    d = money * t;
+    if(t > 60 && t<= 120)
+    d = money * t*1.33;
+    printf("%.1f",d);
+}
 

5414d44285b1f783fd079cb69f2090f7764ba857
112310204 <146065554+li-weisheng@users.noreply.github.com>
2023-10-31 22:40:40 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,15 +1,23 @@
+
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    int money , t;
-    float d;
-    scanf("%d%d",&money,&t);
-    if(t <= 60)
-    d = money * t;
-    if(t > 60 && t<= 120)
-    d = money * t*1.33;
-    printf("%.1f",d);
+int main() {
+    double hourlyWage;
+    int hoursWorked;
+    double totalPayment;
+    scanf("%d", &hoursWorked);
+    scanf("%lf", &hourlyWage);
+
+    if (hoursWorked <= 60) {
+        totalPayment = hoursWorked * hourlyWage;
+    } else if (hoursWorked <= 120) {
+        totalPayment = 60 * hourlyWage + (hoursWorked - 60) * hourlyWage * 1.33;
+    } else {
+        totalPayment = 60 * hourlyWage + 60 * hourlyWage * 1.33 + (hoursWorked - 120) * hourlyWage * 1.66;
+    }
+
+ 
+    printf(" %.2lf\n", totalPayment);
+
+    return 0;
 }
-

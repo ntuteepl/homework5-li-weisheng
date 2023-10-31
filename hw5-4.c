ba6631ac49b5d3c3e773265976fb96468f57e0ea
112310204 <146065554+li-weisheng@users.noreply.github.com>
2023-10-31 23:11:50 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int hour, minute;
+    double angle;
+    scanf("%d", &hour);
+    scanf("%d", &minute);
+    double hourAngle = (hour % 12) * 30 + minute * 0.5;
+    double minuteAngle = minute * 6;
+    angle = fabs(hourAngle - minuteAngle);
+    if (angle > 180) {
+        angle = 360 - angle;
+    }
+    printf("%.3f \n", angle);
+
+    return 0;
+}
 
